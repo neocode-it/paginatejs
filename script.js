f7c412ef69c06566efdc749e5c90e316b992661b
Martin Heini <maheini35@me.com>
2025-03-08 23:45:05 +0100

    feat: add core function for content processing

--- script.js
+++ script.js
@@ -30,7 +30,52 @@ class Renderer {
 
     // Create current domtree
     this.targetParent = this.page;
-  }
+  processContent(parentNode = this.content) {
+    // iterate through all direct children
+    for (let i = 0; i < parentNode.childNodes.length; i++) {
+      const node = parentNode.childNodes[i];
 
+      let height = this.insertAndCheckNode(node);
+
+      // Overflow present?
+      if (height > this.maxHeight) {
+        // Remove overflowing node
+        this.removeLastChildNode();
+
+        // Does node has children?
+        if (node.hasChildNodes()) {
+          let prevTargetParent = this.targetParent;
+
+          // increse current dom depth
+          // Add node shallow again
+          let newParent = node.cloneNode(false);
+          this.targetParent.appendChild(newParent);
+          this.targetParent = newParent;
+          this.parentList.push(node);
+
+          this.processContent(node);
+
+          // remove current dom depth
+          this.parentList.pop();
+          this.targetParent = this.targetParent.parentNode;
+
+          // In case there is none, There has been a page-break and the children are on the new page.
+          // -> No need to render the (empty) wrapping parent in this case then..
+          if (!newParent.hasChildNodes()) {
+            newParent.remove();
+          }
+        } else {
+          // There's no further way to break down the children, we create a Break page
+          this.newPage();
+
+          let height = this.insertAndCheckNode(node);
+          if (height > this.maxHeight) {
+            console.log(
+              "Element cannot be rendered to page, does overflow by itself..."
+            );
+          }
+        }
+      }
+    }
   }
 
