ed2a98033db1f2e1eaf0e851ed453d0ba256c586
Martin Heini <maheini35@me.com>
2025-03-08 23:42:02 +0100

    feat: contstructor of class

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  renderer = new Renderer(document.querySelector("template").content);
+
+  const start = performance.now();
+
+  renderer.processContent();
+
+  const end = performance.now();
+  const timeTaken = end - start;
+  console.log(`Time taken: ${timeTaken}ms`);
+});
+
+class Renderer {
+  constructor(content, renderTo = document.body) {
+    this.content = content;
+    this.renderTo = renderTo;
+
+    // Current page targetElement to copy nodes into
+    this.targetParent = this.page;
+    // Dom depth which will be added in case of a page-break
+    this.parentList = [];
+    this.maxHeight = 300;
+  }
+
