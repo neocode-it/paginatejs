f8ebbaaf9298ae72031fbc609228881af5b87d49
Martin Heini <maheini35@me.com>
2025-03-22 01:26:47 +0100

    feat: change core funct. and iterate through all elements (required for break-after e.g.

--- src/objects/renderer.js
+++ src/objects/renderer.js
@@ -23,43 +23,44 @@ export class Renderer {
     for (let i = 0; i < parentNode.childNodes.length; i++) {
       const node = parentNode.childNodes[i];
 
-      let height = this.insertAndCheckNode(node);
+      if (node.hasChildNodes()) {
+        // increse current dom depth
+        // Add node shallow again
+        let newParent = node.cloneNode(false);
+        this.targetParent.appendChild(newParent);
 
-      // Overflow present?
-      if (height > this.maxHeight) {
-        // Remove overflowing node
-        this.removeLastChildNode();
+        this.targetParent = newParent;
+        this.parentList.push(node);
 
-        // Does node has children?
-        if (node.hasChildNodes()) {
-          let prevTargetParent = this.targetParent;
+        this.processContent(node);
 
-          // increse current dom depth
-          // Add node shallow again
-          let newParent = node.cloneNode(false);
-          this.targetParent.appendChild(newParent);
-          this.targetParent = newParent;
-          this.parentList.push(node);
+        // remove current dom depth
+        this.parentList.pop();
+        this.targetParent = this.targetParent.parentNode;
 
-          this.processContent(node);
+        // In case there is none, There has been a page-break and the children are on the new page.
+        // -> No need to render the (empty) wrapping parent in this case then..
+        if (!newParent.hasChildNodes()) {
+          newParent.remove();
+        }
+      } else {
+        let height = this.insertAndCheckNode(node);
 
-          // remove current dom depth
-          this.parentList.pop();
-          this.targetParent = this.targetParent.parentNode;
+        if (height > this.maxHeight) {
+          // Remove overflowing node
+          this.removeLastChildNode();
 
-          // In case there is none, There has been a page-break and the children are on the new page.
-          // -> No need to render the (empty) wrapping parent in this case then..
-          if (!newParent.hasChildNodes()) {
-            newParent.remove();
-          }
-        } else {
           // There's no further way to break down the children, we create a Break page
           this.newPage();
 
+          // Re-insert this node
           let height = this.insertAndCheckNode(node);
+
+          // Still overflowing? Element can't be broken even more... MAYDAY :)
           if (height > this.maxHeight) {
             console.log(
-              "Element cannot be rendered to page, does overflow by itself..."
+              "Element cannot be rendered to page, does overflow by itself..." +
+                node.textContent
             );
           }
         }
