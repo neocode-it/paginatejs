d31d590ebe96ff3ae45dab2db5dde2ace64583fc
Martin Heini <maheini35@me.com>
2025-03-23 22:17:55 +0100

    feat: Add base paginatejs styles

--- src/objects/skeleton.js
+++ src/objects/skeleton.js
@@ -1,1 +1,38 @@
-export class Skeleton {}
+export class Skeleton {
+  static getBaseStyleElement() {
+    const style = document.createElement("style");
+    style.innerHTML = `
+          *, ::after, ::before {
+            box-sizing: border-box;
+          }
+          .paginatejs-pages {
+            display: flex;
+            flex-direction: column;
+            gap: 0.5cm;
+          }
+          .page {
+            width: 210mm;
+            height: 297mm;
+          }
+          .page .header,
+          .page .footer {
+            width: 100%;
+            height: 2cm;
+          }
+          .page .content {
+            width: 100%;
+            height: 100%;
+          }
+          @media print {
+            .paginatejs * {
+              break-after: unset !important;
+              break-before: unset !important;
+              break-inline: unset !important;
+            }
+            .paginatejs{
+              gap: 0px;
+            }
+          }
+        `;
+    return style;
+  }
