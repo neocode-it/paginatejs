d0f88955a406f41cc7436874a12eac6aedd9d28f
Martin Heini <maheini35@me.com>
2025-03-23 22:24:17 +0100

    feat: Add method to calculate & fix page partial heights

--- src/objects/page.js
+++ src/objects/page.js
@@ -1,1 +1,40 @@
-export class Page {}
+  /**
+   * Calculates & locks the page heights of header, content and footer
+   * in order to prevent accidental changes after content has been added
+   *
+   * @param {HTMLElement} page - The (empty) rendered page
+   * @param {HTMLElement} header - The (empty) header element of the page
+   * @param {HTMLElement} conten - The (empty) content element of the page
+   * @param {HTMLElement} footer - The (empty) footer element of the page
+   *
+   * @returns {null}
+   */
+  calculateAndLockHeights(page, header, content, footer) {
+    const pageWidth = page.offsetWidth;
+    page.style.width = pageWidth + "px";
+    page.style.maxWidth = pageWidth + "px";
+
+    const pageHeight = page.offsetHeight;
+    page.style.height = pageHeight + "px";
+    // Add little offset of .4px
+    // in order to prevent empty last page in Firefox
+    page.style.maxHeight = pageHeight - 0.4 + "px";
+
+    const headerHeight = header.offsetHeight;
+    header.style.height = headerHeight + "px";
+    header.style.maxHeight = headerHeight + "px";
+
+    const footerHeight = footer.offsetHeight;
+    footer.style.height = footerHeight + "px";
+    footer.style.maxHeight = footerHeight + "px";
+
+    const contentHeight = content.offsetHeight;
+    content.style.height = contentHeight + "px";
+    content.style.maxHeight = contentHeight + "px";
+
+    this.height = pageHeight;
+    this.width = pageWidth;
+    this.headerHeight = headerHeight;
+    this.contentHeight = contentHeight;
+    this.footerHeight = footerHeight;
+  }
