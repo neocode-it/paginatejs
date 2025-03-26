4fcf24c6f563b6055bcf890d3f81f18c5a4095cf
Martin Heini <maheini35@me.com>
2025-03-26 23:41:36 +0100

    feat: implement core function preparePrintLayout

--- src/objects/documentlayoutmanager.js
+++ src/objects/documentlayoutmanager.js
@@ -1,1 +1,16 @@
-class DocumentLayoutManager {}
+/**
+ * DocumentLayoutManager is responsible for general tasks such as:
+ * - Generate base wrapper for paginate.js pages
+ * - Insert base css required for paginate.js
+ * - Add media print settings for paginate.js
+ * - Responsible to calculate and create new pages with preset layout (size & style)
+ */
+export class DocumentLayoutManager {
+  constructor(parentElement) {
+    this.parentElement = parentElement;
+  }
+
+  preparePrintLayout() {
+  }
+
+}
