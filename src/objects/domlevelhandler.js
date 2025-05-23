cce15eea6ed855f054f6776745ba531f8fec1baf
Martin Heini <maheini35@me.com>
2025-05-24 00:01:32 +0200

    feat: implement addToDomlevel method

--- src/objects/domlevelhandler.js
+++ src/objects/domlevelhandler.js
@@ -3,4 +3,22 @@ export class DomLevelHandler {
     this.domLevels = [];
     this.target = null;
   }
+  addToDomLevel(element) {
+    let before = [];
+    let after = [];
+
+    // Add table header & footer as element too if required
+    if (element.tagName === "TBODY") {
+      this.#handleTables(before, element, after);
+    }
+
+    const level = {
+      before: before,
+      main: element,
+      after: after,
+    };
+
+    this.domLevels.push(level);
+  }
+
 }
