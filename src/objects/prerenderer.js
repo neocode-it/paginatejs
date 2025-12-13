127eed465180cbdc97758a0806663a3a70529975
maheini <maheini35@me.com>
2025-12-14 00:01:53 +0100

    feat: Add PreRenderer class to pre-calculate the content

---
+++ src/objects/prerenderer.js
@@ -0,0 +1,14 @@
+import { Page } from "./page";
+
+const contentOptions = {
+  breakBefore: "breakbefore",
+  breakAfter: "breakafter",
+  noBreakInside: "nobreak",
+  verticalLayout: "vertical",
+  keepLayout: "keeplayout",
+};
+
+/**
+ * PreRenderer is responsible for analyzing the content and setting attributes for the renderer.
+ */
+export class PreRenderer {}
